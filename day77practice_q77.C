00359d97eca8478d197addc2611dcd9890f6fb15
Saumya <saumya.23646@stu.upes.ac.in>
2026-04-18 22:03:40 +0530

    Check if Graph is Connected

---
+++ day77practice_q77.C
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* createNode(int val) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = val;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void addEdge(Node** adj, int u, int v) {
+    Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+    
+    newNode = createNode(u);
+    newNode->next = adj[v];
+    adj[v] = newNode;
+}
+
+void dfs(Node** adj, int* visited, int node) {
+    visited[node] = 1;
+    
+    Node* temp = adj[node];
+    while (temp != NULL) {
+        int neighbor = temp->data;
+        if (!visited[neighbor]) {
+            dfs(adj, visited, neighbor);
+        }
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, m;
+    
+    printf("Enter number of vertices and edges: ");
+    scanf("%d %d", &n, &m);
+    
+    Node** adj = (Node**)malloc((n + 1) * sizeof(Node*));
+    for (int i = 1; i <= n; i++) {
+        adj[i] = NULL;
+    }
+    
+    printf("Enter %d edges:\n", m);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        addEdge(adj, u, v);
+    }
+    
+    int* visited = (int*)calloc(n + 1, sizeof(int));
+    
+    // Start DFS from vertex 1
+    dfs(adj, visited, 1);
+    
+    int connected = 1;
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            connected = 0;
+            break;
+        }
+    }
+    
+    if (connected) {
+        printf("CONNECTED\n");
+    } else {
+        printf("NOT CONNECTED\n");
+    }
+    
+    // Free memory
+    for (int i = 1; i <= n; i++) {
+        Node* temp = adj[i];
+        while (temp != NULL) {
+            Node* toFree = temp;
+            temp = temp->next;
+            free(toFree);
+        }
+    }
+    free(adj);
+    free(visited);
+    
+    return 0;
+}
